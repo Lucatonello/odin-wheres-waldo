87016b154dcd4e021361aaaef53ea26381764db3
Lucatonello <lucatonellow@gmail.com>
2024-09-21 14:52:42 -0300

    recognition of correct / incorrect guesses with backend

--- src/routes/Game.jsx
+++ src/routes/Game.jsx
@@ -5,6 +5,7 @@ function Game() {
     const [clientX, setClientX] = useState("");
     const [clientY, setClientY] = useState("");
     const [showDD, setShowDD] = useState(false);
+    const [result, setResult] = useState("");
 
     function mousePos(event) {
           setClientX(event.clientX);
@@ -26,6 +27,24 @@ function Game() {
         console.log(event.clientX, event.clientY);
       }
 
+    const handleGuess = async (character) => {
+        try {
+            const response = await fetch(`http://localhost:3000/guess/${character}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ clientX, clientY })
+            });
+            const fetchResult = await response.json();
+            setShowDD(false);  
+            setResult(fetchResult) 
+            console.log(fetchResult)
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     const dropdownStyle = {
         position: 'absolute',
         left: clientX,
@@ -36,16 +55,21 @@ function Game() {
 
     return (
         <div onClick={mousePos}>
+            {result && (
+                <h1>{result.message}</h1>
+            )}
             <div className='imgContainer'>
                 <img src="https://images2.alphacoders.com/925/925901.jpg" alt="waldo" />
             </div>
             {showDD && (
                 <div style={dropdownStyle}>
-                    <button onClick={() => alert('You selected Waldo')}>Waldo</button>
-                    <button onClick={() => alert('You selected Wizzard')}>Wizzard</button>
+                    <button onClick={() => handleGuess('waldo')}>Waldo</button>
+                    <button >Wizzard</button>
+                
                 </div>
             
             )}
+            
         </div>
     );
     
