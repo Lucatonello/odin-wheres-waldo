42482bf1d5c12f6420d942f49551116c105e2a7a
Lucatonello <lucatonellow@gmail.com>
2024-09-21 12:47:15 -0300

    dropdown displayed on click, removd on click away

--- src/routes/Game.jsx
+++ src/routes/Game.jsx
@@ -1,41 +1,54 @@
 import '../Game.css';
-import { useState } from React;
+import { useState } from 'react';
 
 function Game() {
     const [clientX, setClientX] = useState("");
     const [clientY, setClientY] = useState("");
+    const [showDD, setShowDD] = useState(false);
 
-    function printMousePos(event) {
-        let cords =
-          "clientX: " + event.clientX +
-          " - clientY: " + event.clientY;
-          
-          setClientX(event.clientX)
-          console.log(cords);
+    function mousePos(event) {
+          setClientX(event.clientX);
+          setClientY(event.clientY);
+
+          const imgContainer = document.querySelector('.imgContainer');
+          const bounds = imgContainer.getBoundingClientRect();
+
+          if (
+            event.clientX >= bounds.left &&
+            event.clientX <= bounds.right &&
+            event.clientY >= bounds.top &&
+            event.clientY <= bounds.bottom
+        ) {
+            setShowDD(true);
+        } else {
+            setShowDD(false);
+        }
+        console.log(event.clientX, event.clientY);
       }
-      document.addEventListener("click", printMousePos);
+
+    const dropdownStyle = {
+        position: 'absolute',
+        left: clientX,
+        top: clientY,
+        display: 'flex',
+        flexDirection: 'column'
+    };
+
     return (
-        <div>
+        <div onClick={mousePos}>
             <div className='imgContainer'>
                 <img src="https://images2.alphacoders.com/925/925901.jpg" alt="waldo" />
             </div>
-
-            <select name='pointer' id='dropdown' style={dropdownStyle.dropdown}>
-                <option value="waldo">Waldo</option>
-                <option value="wizzard">Wizzard</option>
-            </select>
-
-        </div>
-    )
-}
-
-const dropdownStyle = {
-    dropdown: {
-        position: 'absolute',
-        left: ((e) => {e.clientX}),
-        top: ((e) => {e.clientY}),
+            {showDD && (
+                <div style={dropdownStyle}>
+                    <button onClick={() => alert('You selected Waldo')}>Waldo</button>
+                    <button onClick={() => alert('You selected Wizzard')}>Wizzard</button>
+                </div>
             
-    }
+            )}
+        </div>
+    );
+    
 }
 
 export default Game;
